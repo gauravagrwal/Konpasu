cd46716d53ab5c84a0dee9a2cb09324f86eb8030
Gaurav Agrawal <gauravag.1005@gmail.com>
2022-01-12 22:54:18 +0530

    feat: adding App Center’s SDK
    - Microsoft.AppCenter.Analytics
    - Microsoft.AppCenter.Crashes

--- Konpasu/App.xaml.cs
+++ Konpasu/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using Xamarin.Forms;
+﻿using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using Xamarin.Forms;
 
 namespace Konpasu
 {
@@ -13,6 +16,9 @@ namespace Konpasu
 
         protected override void OnStart()
         {
+            AppCenter.Start("android=3f4b08d0-03c3-4da2-984d-d23078550a9b;" +
+                "ios=ed66c427-8863-4a3e-958e-e1240f76a20a;",
+                typeof(Analytics), typeof(Crashes));
         }
 
         protected override void OnSleep()
