ecaf528166b5e12e290db940df31583c547a930f
Gaurav Agrawal <gauravag.1005@gmail.com>
2022-01-14 19:38:08 +0530

    [Android] adding app icon

--- Konpasu.Android/MainActivity.cs
+++ Konpasu.Android/MainActivity.cs
@@ -6,7 +6,7 @@ using Android.Runtime;
 
 namespace Konpasu.Droid
 {
-    [Activity(Label = "Konpasu", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    [Activity(Label = "Konpasu", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
