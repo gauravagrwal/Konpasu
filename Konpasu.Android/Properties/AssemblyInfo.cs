322b04b9ae18aa356b10fabadb56e2a826d3a79a
Gaurav Agrawal <gauravag.1005@gmail.com>
2022-02-20 23:37:44 +0530

    [Android] removing unnecessary permissions

--- Konpasu.Android/Properties/AssemblyInfo.cs
+++ Konpasu.Android/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
-﻿using Android.App;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -23,7 +22,3 @@ using System.Runtime.InteropServices;
 //      Revision
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-
-// Add some common permissions, these can be removed if not needed
-[assembly: UsesPermission(Android.Manifest.Permission.Internet)]
-[assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)]
